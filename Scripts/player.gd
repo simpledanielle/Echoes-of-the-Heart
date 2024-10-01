4cabee104f6de629fe80fcd111355eaa4b145df8
DanielleBunn <daniellebunn53@gmail.com>
2024-10-01 19:18:25 -0400

    Initial commit for Godot Platformer

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 400  # speed in pixels/sec
+var speed = 300  # speed in pixels/sec
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
